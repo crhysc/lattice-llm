8c740cafa8bd22f465df9081e282b75bc3b690e7
Charles Campbell <crc00042@mix.wvu.edu>
2025-03-31 16:50:03 -0400

    fix dvc download stage

--- data/download.sh
+++ data/download.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Set default output directory or use the one passed as argument
-OUTDIR="${1:-./raw}"
+INFILE="data/urls.txt"
+OUTDIR="data/raw/"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTDIR"
 
 # Make sure urls.txt exists
-if [ ! -f urls.txt ]; then
+if [ ! -f $INFILE ]; then
   echo "urls.txt not found!"
   exit 1
 fi
 
 # Total number of URLs
-total_urls=$(wc -l < urls.txt)
+total_urls=$(wc -l < $INFILE)
 count=0
 
 # Loop through each URL in the file
@@ -26,12 +27,12 @@ while read -r url; do
 
   # Download the file
   echo "[$count/$total_urls] Downloading $filename to $OUTDIR..."
-  wget -O "$filepath" "$url"
+  wget -q -O "$filepath" "$url"
 
   # Increment count
   count=$((count + 1))
 
-done < urls.txt
+done < $INFILE
 
 echo "All downloads complete. Files saved to: $OUTDIR"
 
